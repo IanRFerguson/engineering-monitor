27a3e9feb3deb1c05e46fd1ae9f3346108814b4b
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-05-05 19:30:46 -0400

    clean up

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,8 +1,21 @@
 set -e
 
+if [ ! -f .env ]; then
+    echo "ERROR! .env file not found"
+    exit 1
+fi
+
 # Build React app
 echo "Building React app..."
-cd frontend && npm run build
+cd frontend 
+
+# Check for node modules
+if [ ! -d node_modules/ ]; then
+    echo "Running installs..."
+    npm install
+fi
+
+npm run build
 cd ..
 
 echo "Building Docker container..."
